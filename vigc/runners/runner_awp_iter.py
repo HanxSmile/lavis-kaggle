5af312c16ee65f7cd6997a83c719842c1dda2467
hanx_smile <hanx_smile@icloud.com>
2023-09-26 23:26:40 +0800

    debug

--- vigc/runners/runner_awp_iter.py
+++ vigc/runners/runner_awp_iter.py
@@ -8,6 +8,8 @@
 from vigc.common.registry import registry
 from vigc.runners.runner_iter import RunnerIter
 from vigc.common.awp import AWP
+import logging
+import torch
 
 
 @registry.register_runner("runner_awp_iter")
@@ -31,7 +33,7 @@ class RunnerAwpIter(RunnerIter):
         super().__init__(cfg, task, model, datasets, job_id)
 
         self._awp_model = AWP(
-            self.model.module,
+            self.model,
             self.optimizer,
             adv_lr=cfg.run_cfg.adv_lr,
             adv_eps=cfg.run_cfg.adv_eps,
@@ -56,4 +58,36 @@ class RunnerAwpIter(RunnerIter):
             log_freq=self.log_freq,
             accum_grad_iters=self.accum_grad_iters,
         )
+
+    @property
+    def optimizer(self):
+        # TODO make optimizer class and configurations
+        if self._optimizer is None:
+            num_parameters = 0
+            p_wd, p_non_wd = [], []
+            for n, p in self.model.named_parameters():
+                if not p.requires_grad:
+                    continue  # frozen weights
+                if p.ndim < 2 or "bias" in n or "ln" in n or "bn" in n:
+                    p_non_wd.append(p)
+                else:
+                    p_wd.append(p)
+                num_parameters += p.data.nelement()
+            logging.info("number of trainable parameters: %d" % num_parameters)
+            optim_params = [
+                {
+                    "params": p_wd,
+                    "weight_decay": float(self.config.run_cfg.weight_decay),
+                },
+                {"params": p_non_wd, "weight_decay": 0},
+            ]
+            beta2 = self.config.run_cfg.get("beta2", 0.999)
+            self._optimizer = torch.optim.AdamW(
+                optim_params,
+                lr=float(self.config.run_cfg.init_lr),
+                weight_decay=float(self.config.run_cfg.weight_decay),
+                betas=(0.9, beta2),
+            )
+
+        return self._optimizer
 
