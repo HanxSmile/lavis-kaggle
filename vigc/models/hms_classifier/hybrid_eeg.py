1c5bdc8e02f3fd85c7294f3b7ffbe562df758564
Xiao Han <hanx.han@shopee.com>
2024-02-04 15:14:54 +0800

    update two reference models

---
+++ vigc/models/hms_classifier/hybrid_eeg.py
@@ -0,0 +1,209 @@
+import torch
+from torch import nn
+
+from einops import rearrange, repeat
+
+
+# helpers
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+# classes
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.norm = nn.LayerNorm(dim)
+
+        self.attend = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout),
+                FeedForward(dim, mlp_dim, dropout=dropout)
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
+
+
+class ResNet_1D_Block(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, downsampling):
+        super(ResNet_1D_Block, self).__init__()
+        self.bn1 = nn.BatchNorm1d(num_features=in_channels)
+        self.relu = nn.ReLU(inplace=False)
+        self.dropout = nn.Dropout(p=0.0, inplace=False)
+        self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                               stride=stride, padding=padding, bias=False)
+        self.bn2 = nn.BatchNorm1d(num_features=out_channels)
+        self.conv2 = nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size,
+                               stride=stride, padding=padding, bias=False)
+        self.maxpool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+        self.downsampling = downsampling
+
+    def forward(self, x):
+        identity = x
+
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.conv2(out)
+
+        out = self.maxpool(out)
+        identity = self.downsampling(x)
+
+        out += identity
+        return out
+
+
+class EEGNet(nn.Module):
+
+    def __init__(self, kernels, mlp_dim=96, heads=8, dim_head=16, in_channels=20, fixed_kernel_size=17, num_classes=6,
+                 pool="mean", emb_dropout=0.1, dropout=0.1, depth=2):
+        super(EEGNet, self).__init__()
+        self.kernels = kernels
+        self.planes = 24
+        self.parallel_conv = nn.ModuleList()
+        self.in_channels = in_channels
+
+        for i, kernel_size in enumerate(list(self.kernels)):
+            sep_conv = nn.Conv1d(in_channels=in_channels, out_channels=self.planes, kernel_size=(kernel_size),
+                                 stride=1, padding=0, bias=False, )
+            self.parallel_conv.append(sep_conv)
+
+        self.bn1 = nn.BatchNorm1d(num_features=self.planes)
+        self.relu = nn.ReLU(inplace=False)
+        self.conv1 = nn.Conv1d(in_channels=self.planes, out_channels=self.planes, kernel_size=fixed_kernel_size,
+                               stride=2, padding=2, bias=False)
+        self.block = self._make_resnet_layer(kernel_size=fixed_kernel_size, stride=1, padding=fixed_kernel_size // 2)
+        self.bn2 = nn.BatchNorm1d(num_features=self.planes)
+
+        num_patches = 39
+
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, self.planes))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, self.planes))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(self.planes, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Linear(self.planes, num_classes)
+
+    def _make_resnet_layer(self, kernel_size, stride, blocks=9, padding=0):
+        layers = []
+        downsample = None
+        base_width = self.planes
+
+        for i in range(blocks):
+            downsampling = nn.Sequential(
+                nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+            )
+            layers.append(ResNet_1D_Block(in_channels=self.planes, out_channels=self.planes, kernel_size=kernel_size,
+                                          stride=stride, padding=padding, downsampling=downsampling))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out_sep = []
+        for i in range(len(self.kernels)):
+            sep = self.parallel_conv[i](x)
+            out_sep.append(sep)
+
+        out = torch.cat(out_sep, dim=2)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv1(out)
+        out = self.block(out)  # [b, 24, 39]
+        out = self.bn2(out)
+        x = self.relu(out).permute(0, 2, 1)  # [b, 39, 24]
+        b, n = x.shape[:2]
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b=b)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        x = x.mean(dim=1) if self.pool == 'mean' else x[:, 0]
+
+        x = self.to_latent(x)
+        return self.mlp_head(x)
+
+
+if __name__ == '__main__':
+    import gc
+
+    iot = torch.randn(2, 8, 10000)  # .cuda()
+    model = EEGNet(kernels=[3, 5, 7, 9], in_channels=8, fixed_kernel_size=5, num_classes=6)  # .cuda()
+    output = model(iot)
+    print(output.shape)
+
+    del iot, model
+    gc.collect()
