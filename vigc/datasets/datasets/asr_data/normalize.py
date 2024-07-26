e6141e1af52ff4dff8bd98720fe7e16b8c7acd22
hanx_smile <hanx_smile@icloud.com>
2024-07-26 20:17:13 +0800

    update normalize

--- vigc/datasets/datasets/asr_data/normalize.py
+++ vigc/datasets/datasets/asr_data/normalize.py
@@ -9,13 +9,18 @@ def normalize(text, language=None):
     if language == "fa":
         text = PersianNormalizer.normalize(text=text, zwnj="\u200c", clean_url=True, remove_emoji=True)
     text = text.lower()
-    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'[^\w\s]', ' ', text)
+    text = re.sub(
+        r"\s{2,}",
+        " ",
+        text,
+    )
     text = "".join([unicodedata.normalize('NFC', _) for _ in text])
-    return text
+    return text.strip()
 
 
 if __name__ == '__main__':
     text = """
-    خود بیماری‌های عفونی
+    甘納豆はどう？カステラも、パンもあるよ、などと言って騒ぎますので、
     """
-    print(normalize(text, language="fa"))
+    print(normalize(text, language="ja"))
