18c45c36ac21b55e4a87a6ea6c09f950c4f9b0e6
hanx_smile <hanx_smile@icloud.com>
2024-07-29 14:30:23 +0800

    some update

--- vigc/projects/asr/convert_hf_to_faster_whisper.sh
+++ vigc/projects/asr/convert_hf_to_faster_whisper.sh
@@ -1,1 +1,13 @@
-ct2-transformers-converter --model /mnt/data/hanxiao/MyCode/lavis-kaggle/vigc/output/whisper_asr_el/20240628194/whisper-large-greek-best  --output_dir /mnt/data/hanxiao/MyCode/lavis-kaggle/vigc/output/whisper_asr_el/20240628194/faster-whisper-large-greek-best  --quantization float16
+src_path=/mnt/data/hanxiao/MyCode/lavis-kaggle/vigc/output/whisper_fleurs_new_ja/20240727030
+dst_path=/mnt/data/hanxiao/models/audio/hx-asr/models
+lang=ja
+
+cd /mnt/data/hanxiao/MyCode/lavis-kaggle
+
+python vigc/projects/asr/convert_ckpt_to_hf.py  --src-path ${src_path}/checkpoint_best.pth  --dst-path ${src_path}/whisper-large-${lang}
+cp /mnt/data/hanxiao/models/audio/whisper-large-v3-tokenizer/*  ${src_path}/whisper-large-${lang}
+
+ct2-transformers-converter --model ${src_path}/whisper-large-${lang}  --output_dir ${src_path}/faster-whisper-large-${lang}  --quantization float16
+
+cp /mnt/data/hanxiao/models/audio/whisper-large-v3-tokenizer/*   ${src_path}/faster-whisper-large-${lang}
+mv ${src_path}/faster-whisper-large-${lang}  ${dst_path} 
